b4b3f7fd49c1d20e5e28ee047e92ced11e87ad29
Moch. Lutfi <kapten.lufi@gmail.com>
2019-07-31 14:35:12 +0700

    Build server (#3)
    * remove submodule
    
    * added themes
    
    * delete public
    
    * remove routing
    
    * replace using hugo extended version
    
    * test
    
    * added install stdlib
    
    * try export linked
    
    * install libstdc
    
    * remove gitinfo

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+curl -L -O https://github.com/gohugoio/hugo/releases/download/v0.56.2/hugo_extended_0.56.2_Linux-64bit.tar.gz
+tar -xzf hugo_extended_0.56.2_Linux-64bit.tar.gz
+
+curl -L -O ftp://ftp.pbone.net/mirror/li.nux.ro/download/nux/dextop/el6/x86_64/chrome-deps-stable-3.11-1.x86_64.rpm
+rpm -i --badreloc --noscripts --relocate /opt/google/chrome=$HOME chrome-deps-stable-3.11-1.x86_64.rpm
+export LD_LIBRARY_PATH=$HOME/lib:$LD_LIBRARY_PATH
+
+./hugo -v
